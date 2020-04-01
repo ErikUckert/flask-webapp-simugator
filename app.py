f591d7d04898cedf151fe8d969a7e4014abd42d4
Uckert <Erik.Uckert@avl.com>
2020-04-01 21:33:16 +0200

    beta release

--- app.py
+++ app.py
@@ -1,21 +1,28 @@
 from flask import Flask, render_template, url_for
-from tools import find_user, connect, ip_list
+from tools import find_user, connect, ip_list, load_user_names, get_user_name
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+
     user_stat = []
     for single_ip in ip_list():
-        ip = single_ip
-        connect(ip)
-        path = ip + r'\C$\ProgramData\RealVNC-Service\vncserver.log'
 
-        # check connection status
+        # handle windows logon on every machine
+        connect(single_ip)
         
-        user_stat.append(find_user(path))
+        # get the actual & last users for every machine
+        path = r'\\' + single_ip + r'\C$\ProgramData\RealVNC-Service\vncserver.log'     
+        users = find_user(path)
+        
+        # Translate shortnames into realnames
+        real_current_users = [get_user_name(load_user_names()['users'], user) for user in users[0]]
+        real_last_user = get_user_name(load_user_names()['users'], users[1])
+        user_stat.append([real_current_users, real_last_user])
+
 
     return render_template('index.html', simu_status = zip(ip_list(), user_stat))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='10.16.64.137')
+    app.run(debug=True, host='0.0.0.0')
--- tools.py
+++ tools.py
@@ -1,24 +1,68 @@
 import win32api
 import win32net
 
+import yaml
+
 def find_user(path):
     '''
     Function detects if a user is connected to a given
     IP with VNC Client 
     '''
-    for line in reversed(list(open(path, 'r'))):
-            if 'NtUserSession: auth' in line:
-                user=line[line.find('auth ')+4:].split()[0]
-                var = user
-                break
+    # [[user1, user2], last known user]
+    users = [[], None]
 
-            if 'Connections: disconnected' in line.rstrip():
-                var = 'user logged out'
-                break
+    try:
+        # get the total number of lines for this logfile
+        for line_counter, line in enumerate(list(open(path, 'r'))):
+            line_counter+=1
+
+        # read lines in reversed order and index them
+        for index, line in enumerate(reversed(list(open(path, 'r'))), 1):
+            
+            # ... until a line starts with this string
+            if line.startswith('<13>'):
+
+                # check if a user has logged in and store user name & ip
+                if 'Connections: authenticated:' in line:
+                    user_ip = line[line.find('authenticated: ')+14:].split()[0]
+                    user = line[line.find(', as ')+4:].split()[0]
+
+                    # calculate a starting line for searching a user loggout
+                    # beginning from the current line to the bottom of the logfile
+                    start_line = line_counter - index
+                    user_flag = 'connected'
+
+                    for line in list(open(path, 'r'))[start_line:]:
+
+                        if 'Connections: disconnected: '+ user_ip in line:
+                            user_flag = 'disconnected'
+                            users[1] = user
+                            break
+                        else:
+                            pass
+
+                    if user_flag == 'connected':
+                        users[0].append(user)
+                    else:
+                        pass
+
+                else:
+                    pass
 
             else:
-                var = 'user not found'
-    return var
+                pass
+
+        if users[0] == []:
+            users[0].append('no user connected')
+
+        if users[1] == None:
+            users[1] = 'No previous user'
+
+    except:
+        users[0].append('RealVNC not installed.')
+        users[1] = 'No previous user'
+
+    return users
 
 def connect(ip):
     '''
@@ -30,12 +74,24 @@ def connect(ip):
     password = 'pumapuma'
 
     access_dict = {}
-    access_dict['remote'] = ip + r'\C$'
+    access_dict['remote'] = r'\\' + ip + r'\C$'
     access_dict['password'] = password
     access_dict['username'] = username
-    win32net.NetUseAdd(None, 2, access_dict)
+    try:
+        win32net.NetUseAdd(None, 2, access_dict)
+    except:
+        pass
 
     return None
 
 def ip_list():
-    return [r'\\DEMZKSI110016', r'\\DEMZKWD111245']
+    return [r'REMOTECOMPUTER']
+
+def load_user_names():
+    return yaml.load(open('usernames.yaml', 'r'))
+
+def get_user_name(name_dict, short_name):
+
+	if short_name in name_dict:
+		return name_dict[short_name]
+	return "Name not found..."
